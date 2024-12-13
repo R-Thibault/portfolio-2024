6e6f198ef22d344e6703b4d9e8fc0933d6af12a0
R-Thibault <thibault.rossa@gmail.com>
2024-12-13 09:26:03 +0100

    feat: add my projects bloc on homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import PresentationCard from "@/components/organisms/PresentationCard";
 import JobInfosDisplay from "@/components/organisms/JobInfosDisplay";
+import ProjectsCardsDisplay from "@/components/organisms/ProjectsCardsDisplay";
 
 export default function Home() {
   return (
     <>
       <PresentationCard></PresentationCard>
       <JobInfosDisplay></JobInfosDisplay>
+      <ProjectsCardsDisplay></ProjectsCardsDisplay>
     </>
   );
 }
---
+++ src/types/projectCardType.ts
@@ -0,0 +1,17 @@
+export type ProjectCardType = {
+  id: number;
+  title: string;
+  description: string;
+  imageUrl: string;
+  technologies: string[];
+  githubUrl: string;
+  websiteUrl: string;
+};
+
+export type ProjectCardBlogType = {
+  id: number;
+  title: string;
+  description: string;
+  imageUrl: string;
+  technologies: string[];
+};

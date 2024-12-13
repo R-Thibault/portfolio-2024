470039c7d609cdf0d298af7f3f48064ea211e520
R-Thibault <thibault.rossa@gmail.com>
2024-12-13 08:49:37 +0100

    feat: add presentation card

---
+++ src/components/atoms/ResumeLink.tsx
@@ -0,0 +1,30 @@
+import Link from "next/link";
+import React from "react";
+
+export default function ResumeLink() {
+  return (
+    <Link
+      href=""
+      className="inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 border border-input bg-background shadow-sm hover:bg-accent hover:text-accent-foreground h-9 px-4 py-2"
+    >
+      <span className="font-semibold">Resume</span>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        className="lucide lucide-file-down ml-2 size-5"
+      >
+        <path d="M15 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V7Z"></path>
+        <path d="M14 2v4a2 2 0 0 0 2 2h4"></path>
+        <path d="M12 18v-6"></path>
+        <path d="m9 15 3 3 3-3"></path>
+      </svg>
+    </Link>
+  );
+}

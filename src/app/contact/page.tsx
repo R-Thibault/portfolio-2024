c15b7e766b580a404105f98da161d5d53ffc6f9b
R-Thibault <thibault.rossa@gmail.com>
2024-12-13 11:32:50 +0100

    feat: add contactPage

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,73 @@
+"use client";
+import React from "react";
+import { useForm, ValidationError } from "@formspree/react";
+import ContactSuccess from "@/components/organisms/ContactSucess";
+
+export default function Page() {
+  const formId = process.env.NEXT_PUBLIC_FORMSPREE || "xqkonawp";
+  const [state, handleSubmit] = useForm(formId);
+  if (state.succeeded) {
+    return (
+      <div className="py-6 lg:py-14 px-4 mx-auto max-w-screen-md">
+        <ContactSuccess />
+      </div>
+    );
+  }
+  return (
+    <div className="py-6 lg:py-14 px-4 mx-auto max-w-screen-md">
+      <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-center ">
+        Let&apos;s Talk
+      </h2>
+      <p className="mb-6 text-center text-gray-500">
+        Got an idea or a question? Feel free to reach out using the form below,
+        and I&apos;ll get back to you as soon as possible.
+      </p>
+      <form
+        name="contact"
+        method="POST"
+        onSubmit={handleSubmit}
+        className="space-y-8"
+      >
+        <input type="hidden" name="form-name" value="contact" />
+
+        <div>
+          <label htmlFor="email" className="block mb-1 text-lg font-medium ">
+            Email:
+          </label>
+          <input
+            required
+            type="email"
+            name="email"
+            id="email"
+            className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg  block w-full p-2.5"
+          />
+          <ValidationError prefix="Email" field="email" errors={state.errors} />
+        </div>
+        <div>
+          <label htmlFor="message" className="block mb-1 text-lg font-medium ">
+            Message:
+          </label>
+          <textarea
+            required
+            name="message"
+            id="message"
+            className="block p-2.5 w-full h-28 text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 "
+          ></textarea>
+          <ValidationError
+            prefix="Message"
+            field="message"
+            errors={state.errors}
+          />
+        </div>
+        <div className="flex justify-center">
+          <button
+            type="submit"
+            className="py-3 px-5 text-lg font-medium text-center text-white rounded-lg bg-stone-900 sm:w-fit hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300"
+          >
+            Send
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+}
